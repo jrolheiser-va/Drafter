791f7918b0b703c2072517b3ebbcdc1f7e12feb6
Jesse Rolheiser <jrolheiser@vendasta.com>
2018-04-08 21:51:53 -0600

    Add Table to display all players.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Sort } from '@angular/material';
+import { DataSource } from '@angular/cdk/table';
+
+import { Player, PlayerService } from './player';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  displayedColumns = [
+    'TeamAbbreviation',
+    'FirstName',
+    'LastName',
+    'Points',
+    'GamesPlayed',
+    'Goals',
+    'Assists',
+    'PlusMinus'
+  ];
+  playersSource: DataSource<Player>;
+
+  constructor(private players: PlayerService) {}
+
+  ngOnInit(): void {
+    this.playersSource = {
+      connect: () => this.players.players$,
+      disconnect: () => null
+    };
+  }
+
+  search(searchTerm: string): void {
+    this.players.applySearch(searchTerm);
+  }
+
+  sortPlayers(sort: Sort): void {
+    this.players.applySort(sort);
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatTableModule,
+  MatInputModule,
+  MatSortModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { PlayerService } from './player';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatInputModule,
+    MatSortModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    PlayerService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
